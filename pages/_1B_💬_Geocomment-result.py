73e0f52c3f93901066c0d3bfb00b3668930dea29
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2024-10-25 06:31:35 +0200

    update result point

--- pages/_1B_💬_Geocomment-result.py
+++ pages/_1B_💬_Geocomment-result.py
@@ -15,9 +15,9 @@ client = init_connection()
 
 db=client['EuthMappers_Geocomment']
 collection=db['EuthMappers_Geocomment']
-result_point=gpd.GeoDataFrame(list(collection.find().sort("_id", -1).limit(5)))
-result_point['geometry']=gpd.GeoSeries.from_wkt(gpd['center'])
-result.plot()
+result_point=pd.DataFrame(list(collection.find().sort("_id", -1).limit(5)))
+result_point['geometry']=gpd.GeoSeries.from_wkt(result_point['center'])
+result_point=gpd.Geodataframe(result_point,geometry=result_point['geometry'])
 
 
 
