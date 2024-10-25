45455b9ec37b626f77ba5a14e46e3186bfb99a31
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2024-10-25 11:40:32 +0200

    remove poi_j

--- pages/_1B_💬_Geocomment-result.py
+++ pages/_1B_💬_Geocomment-result.py
@@ -82,7 +82,7 @@ for _, r in result_polygon.iterrows():
     pol_j = pol.to_json()
     pol_j = folium.GeoJson(data=pol_j, style_function=lambda x: {"fillColor": "red","color":"red"})
     folium.Popup(r["comment"]).add_to(pol_j)
-    poi_j=gpd.GeoSeries(r["Points"]).to_json()
+    poi_j=gpd.GeoSeries(r["Point"]).to_json()
     poi_j= folium.GeoJson(data=poi_j)
     pol_j.add_to(map)
     poi_j.add_to(map)
