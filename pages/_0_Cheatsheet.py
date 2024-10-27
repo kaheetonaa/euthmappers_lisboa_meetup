fa1687a3d4f84c50b8d6785bc5ed644dc357fc50
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2024-10-27 17:17:29 +0100

    remove qrcode

--- pages/_0_Cheatsheet.py
+++ pages/_0_Cheatsheet.py
@@ -1,9 +1,5 @@
 import streamlit as st
-from PIL import Image
-import qrcode
-from datetime import datetime
-from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.colormasks import HorizontalGradiantColorMask
+
 st.set_page_config(layout="wide")
 st.markdown("""
 
@@ -52,23 +48,3 @@ st.markdown("""
 
 st.title('📋EUthMappers Humanitarian report and workshop cheatsheet')
 
-generated_qrcodes_path = "generated_qrcodes/"
-
-def generate_qrcode(url):
-    qr = qrcode.QRCode(
-                        version=1,
-                        error_correction=qrcode.constants.ERROR_CORRECT_L,
-                        box_size=10,
-                        border=2
-                        )
-    qr.add_data(url)
-    qr.make(fit=True)
-    img = qr.make_image(image_factory=StyledPilImage, color_mask=HorizontalGradiantColorMask())
-
-    current_ts = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    qrcode_path = generated_qrcodes_path + "qrcode_" + str(current_ts) + ".png"
-    img.save(qrcode_path)
-    return qrcode_path
-
-qrcode_path = generate_qrcode('hello')
-image = Image.open(qrcode_path)
