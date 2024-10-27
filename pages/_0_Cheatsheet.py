c6aeed660f6da8872125aec0fa60b5f771335b72
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2024-10-27 17:33:45 +0100

    update

--- pages/_0_Cheatsheet.py
+++ pages/_0_Cheatsheet.py
@@ -67,4 +67,4 @@ def generate_qr_code(url, fill_color, bg_color):
 qr_img = generate_qr_code('google.com', '#FF0000', '#FFFFFF')
 img_buffer = io.BytesIO()
 qr_io=qr_img.save(img_buffer, format="PNG")
-st.image(qr_io)
+st.image(qr_img)
